ce9fe0cb35012620c6b89ecb930711dd83366790
hasanchamdany <hasanchamdany@gmail.com>
2023-03-22 18:24:02 +0700

    feat(tailwind): add color config tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      inter: ["Inner", "serif"],
+    },
+    extend: {
+      colors: {
+        current: "currentColor",
+        blue: {
+          pale: "#5B80BC",
+          medium: "#219ebc",
+          light: "#8ecae6",
+          dark: "#023047",
+          // nav: "#315FB1",
+        },
+        orange: "#fb8500",
+        "c-yellow": "#ffb703",
+      },
+
+      yellow: {
+        dark: "#E8AF30",
+      },
+      cream: {
+        pale: "#E7E6DD",
+      },
+      grey: {
+        lighter: "#f9f9f9",
+        light: "#ececec",
+      },
+    },
   },
   plugins: [],
 }
