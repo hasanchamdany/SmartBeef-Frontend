21f45c066d67c3f45b58d231968b411ecf5d6d24
rein7703 <69892731+rein7703@users.noreply.github.com>
2023-04-01 21:54:05 +0700

    Added the title and images into the web pages

--- src/pages/history.js
+++ src/pages/history.js
@@ -5,8 +5,12 @@ const history = () => {
   return (
     <>
         <DefaultLayout>
-            <div className="w-full h-screen historyPage-bg">
+            <div className="flex w-full h-screen historyPage-bg">
+              <div className='w-screen justify-center mt-32 mx-8'>
+                <h1 className='text-center font-semibold font-serif-inter text-6xl text-blue-welcome'>Prediction History</h1>
+              </div>
 
+          
             </div>
         </DefaultLayout>
     </>
