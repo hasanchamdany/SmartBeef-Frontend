db21b560946e69ae8f418bb7a72b76c106f2f339
hasanchamdany <hasanchamdany@gmail.com>
2023-04-20 23:06:06 +0700

    fix(mongoose): downgrade mongoose version

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = process.env.MONGO_URL
+const MONGODB_URL = process.env.MONGODB_URL
 
-if(!MONGO_URL) {
+if(!MONGODB_URL) {
     console.error("Invalid environment variable: MONGODB_URL")
 }
 
 export const connectToMongoDB = async () => {
     try{
-        const connection = await mongoose.connect(MONGO_URL, () => {
-            console.log("Mongo Connected")
-        })
+        const connection = await mongoose.connect(process.env.MONGODB_URL)
 
         if(connection.readyState === 1){
             return Promise.resolve(true)
@@ -19,3 +17,31 @@ export const connectToMongoDB = async () => {
         return Promise.reject(error)
     }
 }
+
+// import { MongoClient } from 'mongodb'
+
+// const uri = process.env.MONGODB_URI
+// const options = {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+// }
+
+// let client
+// let clientPromise
+
+// if (!process.env.MONGODB_URI) {
+//   throw new Error('Add Mongo URI to .env.local')
+// }
+
+// if (process.env.NODE_ENV === 'development') {
+//   if (!global._mongoClientPromise) {
+//     client = new MongoClient(uri, options)
+//     global._mongoClientPromise = client.connect()
+//   }
+//   clientPromise = global._mongoClientPromise
+// } else {
+//   client = new MongoClient(uri, options)
+//   clientPromise = client.connect()
+// }
+
+// export default clientPromise
--- src/pages/api/auth/sign-up.js
+++ src/pages/api/auth/sign-up.js
@@ -1,22 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import {hash} from "bcryptjs"
 import { connectToMongoDB } from "@/lib/mongodb"
+// import clientPromise from "@/lib/mongodb";
 import User from "@/models/user"
 
+const MONGODB_URL = process.env.MONGODB_URL
+
 const handler = async (req, res) => {
-    try{
-        connectToMongoDB().catch(err => res.json(err))
-    }catch{
-        console.log(err)
-    }
+    await connectToMongoDB().catch(err => res.json(err))
+    // const client = await clientPromise
+    console.log("connect to MongoDB")
     
+    
+    console.log("mongo URL = ", MONGODB_URL)
 
     if(req.method === "POST"){
         if(!req.body) return res.status(400).json({error: "Data is Missing"})
 
         const {fullName, email, password} = req.body
 
-        const userExists = await  User.findOne({email})
+        const userExists = await User.findOne({email})
 
         if(userExists){
             return res.status(409).json({error: "User Already exists"})
@@ -24,30 +27,53 @@ const handler = async (req, res) => {
             if(password.length < 6) return res.status(409).json({error: "Password too short, should be 6 characters long"})
 
             const hashedPassword = await hash(password, 12)
-
-            User.create({
-                fullName,
-                email,
-                password: hashedPassword 
-            }, (error, data) => {
-                if(error && error instanceof mongoose.Error.ValidationError){
-                    for(let field in error.erros){
-                        const msg = error.errors[field].message
-                        return res.status(409).json({error: msg})
+            console.log("CP sblm user create")
+            try{
+                const user = await User.create({
+                    fullName: req.body.fullName,
+                    email: req.body.email,
+                    password: hashedPassword
+                }, (error, data) => {
+                    if(error && error instanceof mongoose.Error.ValidationError){
+                        for(let field in error.errors){
+                            const msg = error.errors[field].message
+                            return res.status(409).json({error: msg})
+                        }
                     }
-                }
-
-                const user = {
-                    email: data.email,
-                    fullName: data.fullName,
-                    _id: data._id
-                }
-
-                return res.status(201).json({
-                    success: true,
-                    user
+                    res.status(201).json({data})
                 })
-            })
+                
+            }catch(error){
+                console.error("Error creating user", error)
+            }
+            // await User.create({
+            //     fullName,
+            //     email,
+            //     password: hashedPassword 
+            // }, (error, data) => {
+            //     if(error && error instanceof mongoose.Error.ValidationError){
+            //         for(let field in error.erros){
+            //             const msg = error.errors[field].message
+            //             return res.status(409).json({error: msg})
+            //         }
+            //     }
+
+            //     const user = {
+            //         email: data.email,
+            //         fullName: data.fullName,
+            //         _id: data._id
+            //     }
+
+            //     res.status(201).json({
+            //         success: true,
+            //         user
+            //     })
+            // })
+
+            // return res.status(201).json({
+            //     success: true,
+            //     user
+            // })
         }
 
     }else{
