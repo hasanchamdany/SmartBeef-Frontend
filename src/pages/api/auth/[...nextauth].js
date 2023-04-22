782680ac9c357c1e7a15d25092f087efc68b6997
hasanchamdany <hasanchamdany@gmail.com>
2023-04-23 05:48:29 +0700

    feat(auth): trying google provider authentication

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -56,3 +56,20 @@
 // }
 
 // export default NextAuth(options)
+
+
+import NextAuth from "next-auth"
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import GoogleProvider from "next-auth/providers/google"
+import clientPromise from "../../../lib/mongodb"
+
+export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+})
