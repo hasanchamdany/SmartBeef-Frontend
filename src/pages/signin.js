6a9ba9dd2ce91d57430fb288604f650d9256f3c4
hasanchamdany <hasanchamdany@gmail.com>
2023-04-24 08:10:41 +0700

    feat(sign-in): add simple form sign-in for auth test

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
 
 const signin = () => {
   return (
-    <div>signin</div>
-  )
-}
+    <div className="h-screen w-full landingPage-bg flex justify-center items-center ">
+      <div className="w-lg h-lg bg-white rounded-xl px-8 py-6">
+        <form>
+          <label className="font-poppins text-blue-dark font-bold ">
+            {" "}
+            Email
+          </label>
+          <input
+            type="email"
+            className="block bg-white py-2 px-4 border-2 border-blue-medium rounded-xl mb-4"
+            placeholder="xxx@email.com"
+          ></input>
+          <label className="font-poppins text-blue-dark font-bold ">
+            {" "}
+            Password
+          </label>
+          <input
+            type="password"
+            className="block bg-white py-2 px-4 border-2 border-blue-medium rounded-xl mb-4"
+          ></input>
+          <button className="bg-blue-pale py-2 px-4 font-poppins font-bold text-white rounded-lg mr-8">
+            Sign-Up
+          </button>
+          <button className="bg-blue-medium py-2 px-4 font-poppins font-bold text-white rounded-lg">
+            Sign-In
+          </button>
+        </form>
+        <div className="mt-2 flex justify-center">
+          <button className="border-2 border-blue-pale py-2 px-6 rounded-2xl ">
+            Sign-In With Google
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default signin
+export default signin;
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,8 +1,11 @@
+import DefaultLayout from '@/components/DefaultLayout/DefaultLayout'
 import React from 'react'
 
 const signup = () => {
   return (
+    <DefaultLayout>
     <div>signup</div>
+    </DefaultLayout>
   )
 }
 
