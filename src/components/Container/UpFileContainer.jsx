cdcb4a0c061efaca9fb3f3b073cbfe9b21ea77eb
hasanchamdany <hasanchamdany@gmail.com>
2023-05-31 03:53:41 +0700

    feat(spinner): add loading spinner for scanning raw beef image

--- src/components/Container/UpFileContainer.jsx
+++ src/components/Container/UpFileContainer.jsx
@@ -73,14 +73,19 @@ const UpFileContainer = () => {
   return (
     <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       {loading ? (
+        <>
+        <div className="w-full flex justify-center h-full">
         <div
-          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-warning motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          className="flex justify-center h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-warning motion-reduce:animate-[spin_1.5s_linear_infinite]"
           role="status"
         >
           <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
             Loading...
           </span>
         </div>
+        </div>
+        {/* <div className="inline-flex text-lg">Loading</div> */}
+        </>
       ) : (
         <div className="md:flex">
           <div className="md:flex-shrink-0">
